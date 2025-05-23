531f8e4977645fee74643e46aab0905538bfe71d
Sawyerinvi <Eugene_tut@mail.ru>
2025-05-23 07:17:52 +0500

    Gizmo

--- Assets/Scripts/RoadWaypoint.cs
+++ Assets/Scripts/RoadWaypoint.cs
@@ -17,4 +17,25 @@ public class RoadWaypoint : MonoBehaviour
         random = Random.Range(0, availablePoints.Length);
         return availablePoints[random];
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Vector3 start = transform.position;
+
+        foreach (var point in availablePoints)
+        {
+            Vector3 dir = (point.transform.position - start).normalized;
+            Vector3 end = start + dir;
+
+            Gizmos.DrawLine(start, point.transform.position);
+
+            // Вершина стрелки
+            Vector3 right = Quaternion.LookRotation(dir) * Quaternion.Euler(0, 150, 0) * Vector3.forward;
+            Vector3 left = Quaternion.LookRotation(dir) * Quaternion.Euler(0, -150, 0) * Vector3.forward;
+
+            Gizmos.DrawLine(point.transform.position, point.transform.position + right * 0.2f * 10);
+            Gizmos.DrawLine(point.transform.position, point.transform.position + left * 0.2f * 10);
+        }
+    }
 }
