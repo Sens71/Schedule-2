a6602bb9326fe78fd192e4de2bf15a91a524c94e
Sens71 <163084229+Sens71@users.noreply.github.com>
2025-07-21 11:05:34 -0600

    1

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -2,19 +2,5 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
-    private Camera camera;
-    void Start()
-    {
-        camera = Camera.main;
-    }
-
-    
-    void Update()
-    {
-        Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-        if(Physics.Raycast(ray, out RaycastHit hit, 100f))
-        {
-            transform.position = hit.point;
-        }
-    }
+    public GameObject prefab;   
 }
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -2,18 +2,54 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    
+    private Ghost ghost;
+    public float buildingDistance;
+    public GameObject buildPanel;
+    public GameObject plantPanel;
+
     void Start()
     {
         
     }
 
-    public void BuildObject(GameObject building)
-    {
-        Instantiate(building);
+    public void BuildObject(Ghost building)
+    {   
+        if (ghost == null)
+        {
+            ghost = Instantiate(building);
+        }   
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        buildPanel.SetActive(false);
     }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {   
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            buildPanel.SetActive(true);
+        }
+        if (ghost != null)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Instantiate(ghost.prefab, ghost.transform.position, ghost.transform.rotation);
+                Destroy(ghost.gameObject);
+            }
+            RaycastHit hit;
+            Ray screenRay = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+            if(Physics.Raycast(screenRay, out hit, buildingDistance))
+            {
+                ghost.transform.position = hit.point;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            plantPanel.SetActive(true);
+        }
     }
 }
