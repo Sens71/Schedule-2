1d445a2bd9fc14b0a34bf1cbaa87ef2ca79c7e5b
Sens71 <163084229+Sens71@users.noreply.github.com>
2025-08-15 11:04:54 -0600

    1

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,15 +1,11 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
     public Storage storage;
-
-    public GameObject money;
-    public GameObject canSeed;
-    public GameObject kSeed;
-    public GameObject decSeed;
-    public GameObject metSeed;
+    public GameObject [] items;
 
     private TMP_Text text;
     private TMP_Text text1;
@@ -18,45 +14,29 @@ public class Inventory : MonoBehaviour
     private TMP_Text text4;
     void Start()
     {
-        text = money.transform.Find("Amount").GetComponent<TMP_Text>();
-        text1 = canSeed.transform.Find("Amount").GetComponent<TMP_Text>();
-        text2 = kSeed.transform.Find("Amount").GetComponent<TMP_Text>();
-        text3 = decSeed.transform.Find("Amount").GetComponent<TMP_Text>();
-        text4 = metSeed.transform.Find("Amount").GetComponent<TMP_Text>();
-    }
+        CreateSlot(storage.money, items[0]);
+        CreateSlot(storage.decSeed, items[1]);
+        CreateSlot (storage.canSeed, items[2]);
+        CreateSlot(storage.kSeed, items[3]);
+        CreateSlot(storage.metSeed, items[4]);
+        CreateSlot(storage.medPack, items[5]);
 
+    }
+    private void CreateSlot(ItemData itemData, GameObject slot)
+    {
+        var textAmount = slot.transform.Find("Amount").GetComponent<TMP_Text>();
+        var textName = slot.transform.Find("Name").GetComponent<TMP_Text>();
+        var icon = slot.transform.Find("Button").GetComponent<Image>();
+        var bg = slot.transform.Find("Bg").GetComponent<Image>();
+        textAmount.text = itemData.amount.ToString();
+        textName.text = itemData.name.ToString();
+        icon.sprite = itemData.icon;
+        bg.color = itemData.bgColor;
+    }
     private void UpdateUI()
     {
         
-        if (text != null)
-        {
-            text.text = storage.money.ToString();
-            money.SetActive(storage.money > 0);
-        }
         
-        if (text1 != null)
-        {
-            text1.text = storage.canSeed.ToString();
-            canSeed.SetActive(storage.canSeed > 0);
-        }
-        
-        if (text2 != null)
-        {
-            text2.text = storage.kSeed.ToString();
-            kSeed.SetActive(storage.kSeed > 0);
-        }
-        
-        if (text3 != null)
-        {
-            text3.text = storage.decSeed.ToString();
-            decSeed.SetActive(storage.decSeed > 0);
-        }
-        
-        if (text4 != null)
-        {
-            text4.text = storage.metSeed.ToString();
-            metSeed.SetActive(storage.metSeed > 0);
-        }
     }
     void Update()
     {
--- Assets/Scripts/Storage.cs
+++ Assets/Scripts/Storage.cs
@@ -1,11 +1,21 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Storage", menuName = "MyData/Storage")]
 public class Storage : ScriptableObject
 {
-    public int money;
-    public int canSeed;
-    public int kSeed;
-    public int decSeed;
-    public int metSeed; 
+    public ItemData money;
+    public ItemData canSeed;
+    public ItemData kSeed;
+    public ItemData decSeed;
+    public ItemData metSeed;
+    public ItemData medPack;
+}
+[Serializable]
+public class ItemData
+{
+    public string name;
+    public Sprite icon;
+    public int amount;
+    public Color bgColor = Color.yellow;
 }
