ed29a0477e05e286169ed0c895ccf70ba1a3fe06
Sens71 <163084229+Sens71@users.noreply.github.com>
2025-10-18 10:03:56 -0600

    1

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPCController : MonoBehaviour
 {
     private StatsHandler statsHandler;
     private Weapon weapon;
     private float timer;
+    public float _distance;
+    public float viewAngle;
+    private Player player;
+    private NavMeshAgent agent;
+    public LayerMask mask;
     void Awake()
     {
         weapon = GetComponentInChildren<Weapon>();
         weapon.playerWeapon = false;
         statsHandler = GetComponent<StatsHandler>();
+        player = FindAnyObjectByType<Player>();
+        agent = GetComponent<NavMeshAgent>();
     }
     private void OnEnable()
     {
@@ -22,6 +30,7 @@ public class NPCController : MonoBehaviour
 
     void Update()
     {
+        Navigation();
         timer += Time.deltaTime;
         if (timer > 1)
         {
@@ -32,5 +41,19 @@ public class NPCController : MonoBehaviour
     public void Death()
     {
         Destroy(gameObject);
+    }
+    private void Navigation()
+    {
+        var distance = Vector3.Distance(transform.position, player.transform.position);
+        var canSee = !Physics.Linecast(transform.position, player.transform.position, mask);
+        if (distance > _distance && !canSee)
+        {
+            agent.SetDestination(player.transform.position);
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+        }
+
     }
 }
